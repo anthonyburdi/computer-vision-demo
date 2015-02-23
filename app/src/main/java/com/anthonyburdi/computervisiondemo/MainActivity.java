3a4356b2bb8f56faac090154bacd1250fa87e1c6
anthonyburdi <anthonyburdi@gmail.com>
2015-02-22 19:26:08 -0500

    Initial Commit. Added simple camera interaction code to sample boilerplate project.

---
+++ app/src/main/java/com/anthonyburdi/computervisiondemo/MainActivity.java
@@ -0,0 +1,75 @@
+package com.anthonyburdi.computervisiondemo;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+//    ------------------------------------------------------------------------------
+//    FROM https://developer.android.com/training/camera/photobasics.html
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    public ImageView mImageView;
+
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+//    ------------------------------------------------------------------------------
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //    ------------------------------------------------------------------------------
+        // ADDED
+        mImageView = (ImageView) findViewById(R.id.thumbnail_image_view);
+        dispatchTakePictureIntent();
+        //    ------------------------------------------------------------------------------
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
