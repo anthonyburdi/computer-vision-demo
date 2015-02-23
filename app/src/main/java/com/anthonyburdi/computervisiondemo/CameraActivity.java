2338a6f494133671cce8a9d8e6807e7efd7cfa2f
anthonyburdi <anthonyburdi@gmail.com>
2015-02-23 18:33:23 -0500

    Cleanup code. Still debugging camera code. Preview is working but picture button does not work as intended.

--- app/src/main/java/com/anthonyburdi/computervisiondemo/CameraActivity.java
+++ app/src/main/java/com/anthonyburdi/computervisiondemo/CameraActivity.java
@@ -1,45 +1,45 @@
-package com.anthonyburdi.computervisiondemo;
-
-import android.app.Activity;
-import android.hardware.Camera;
-import android.os.Bundle;
-import android.widget.FrameLayout;
-
-/**
- * Created by anthonyburdi on 2/23/15.
- */
-
-//    ------------------------------------------------------------------------------
-// FROM https://developer.android.com/guide/topics/media/camera.html#custom-camera
-public class CameraActivity extends Activity {
-
-    private Camera mCamera;
-    private CameraPreview mPreview;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        // Create an instance of Camera
-        mCamera = getCameraInstance();
-
-        // Create our Preview view and set it as the content of our activity.
-        mPreview = new CameraPreview(this, mCamera);
-        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
-        preview.addView(mPreview);
-    }
-
-    /** A safe way to get an instance of the Camera object. */
-    public static Camera getCameraInstance(){
-        Camera c = null;
-        try {
-            c = Camera.open(); // attempt to get a Camera instance
-        }
-        catch (Exception e){
-            // Camera is not available (in use or does not exist)
-        }
-        return c; // returns null if camera is unavailable
-    }
-
-}
+//package com.anthonyburdi.computervisiondemo;
+//
+//import android.app.Activity;
+//import android.hardware.Camera;
+//import android.os.Bundle;
+//import android.widget.FrameLayout;
+//
+///**
+// * Created by anthonyburdi on 2/23/15.
+// */
+//
+////    ------------------------------------------------------------------------------
+//// FROM https://developer.android.com/guide/topics/media/camera.html#custom-camera
+//public class CameraActivity extends Activity {
+//
+//    private Camera mCamera;
+//    private CameraPreview mPreview;
+//
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        // Create an instance of Camera
+//        mCamera = getCameraInstance();
+//
+//        // Create our Preview view and set it as the content of our activity.
+//        mPreview = new CameraPreview(this, mCamera);
+//        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+//        preview.addView(mPreview);
+//    }
+//
+//    /** A safe way to get an instance of the Camera object. */
+//    public static Camera getCameraInstance(){
+//        Camera c = null;
+//        try {
+//            c = Camera.open(); // attempt to get a Camera instance
+//        }
+//        catch (Exception e){
+//            // Camera is not available (in use or does not exist)
+//        }
+//        return c; // returns null if camera is unavailable
+//    }
+//
+//}
